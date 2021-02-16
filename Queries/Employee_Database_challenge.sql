89ea6c07b3fc0002fe280235bd9474249b4d65be
jdfiel <66696213+jdfiel@users.noreply.github.com>
2021-02-16 01:07:08 -0500

    final query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -4,7 +4,7 @@ Select e.emp_no,
 	ti.title,
 	ti.from_date,
 	ti.to_date
--- INTO retirement_titles
+INTO retirement_titles
 FROM employees as e
 INNER JOIN titles as ti
 ON (e.emp_no = ti.emp_no)
@@ -16,7 +16,7 @@ SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
 last_name,
 title
--- INTO unique_titles
+INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
@@ -25,6 +25,8 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT DESC;
+
+Select SUM (emp_no) from employees;
 
 SELECT DISTINCT ON (emp_no)e.emp_no, 
 	e.first_name, 
@@ -42,5 +44,50 @@ ON (e.emp_no = ti.emp_no)
 WHERE birth_date BETWEEN '1965-01-01' AND '1965-12-31'
 ORDER BY emp_no;
 
+select * from mentorship_eligibilty;
 
+--mentors by title
+SELECT COUNT (title), title
+FROM mentorship_eligibilty
+GROUP BY title
+ORDER BY COUNT DESC;
 
+select * from titles ;
+
+--current by dept
+SELECT d.dept_name, cd.count
+FROM departments as d
+INNER JOIN current_dept as cd
+ON (d.dept_no = cd.dept_no)
+ORDER BY COUNT ;
+
+--current by titles
+SELECT COUNT (title), title
+FROM titles
+GROUP BY title
+ORDER BY COUNT DESC;
+
+--silver tsunami survivors
+SELECT DISTINCT ON (emp_no)e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO not_retiring
+FROM employees as e
+INNER JOIN dept_employees as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE birth_date <= DATE '1965-12-31'
+ORDER BY emp_no;
+
+SELECT COUNT (emp_no) FROM not_retiring;
+
+--survivors by title
+SELECT COUNT (title), title
+FROM not_retiring
+GROUP BY title
+ORDER BY COUNT DESC;
